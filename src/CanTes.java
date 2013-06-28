f9aa5ccd88de351813906fc18652d38537973946
keisuke kamiya <luckey.star123@gmail.com>
2013-06-28 19:20:40 +0900

    もうこれ以上わからん

--- src/CanTes.java
+++ src/CanTes.java
@@ -1,50 +1,37 @@
 import java.awt.*;
 import java.awt.event.*;
-
-class MyCan extends Canvas{
- int x,y;
- Image OI;
- Graphics OG;
- MyCan(){
-  super();
-  setSize(600,400);
-  addMouseMotionListener(new MouseMotionListener() {
-   public void mouseDragged(MouseEvent e) {
-    x=e.getX();
-    y=e.getY();
-    repaint();
-   }
-   public void mouseMoved(MouseEvent e) {
-    x=e.getX();
-    y=e.getY();
-    repaint();
-   }
-  });
- }
- public void update(Graphics g){
-  paint(OG=(OI=createImage(600,400)).getGraphics());
-  g.drawImage(OI,0,0,this);
- }
- public void paint(Graphics g){
-  g.drawLine(300,200,x,y);
- }
-}
-
 class CanTes extends Frame{
- class MW extends WindowAdapter{
-  public void windowClosing(WindowEvent e){
-   System.exit(0);
-  }
- }
- CanTes(){
-  super("");
-  this.addWindowListener(new MW());
-  add(new MyCan());
-  setSize(getPreferredSize());
-  setVisible(1>0);
- }
-
- static{
-  new CanTes();
- }
+    int s=600,x,y,t=300;
+    class W extends WindowAdapter{
+        public void windowClosing(WindowEvent e){System.exit(0);}
+    }
+    CanTes(){
+        addWindowListener(new W());
+        add(new C());
+        setSize(s,s);
+        setVisible(1>0);
+    }
+    class C extends Canvas{
+        C(){
+            x=y=t;
+            setSize(s,s);
+            addMouseMotionListener(new MouseMotionListener(){
+                public void mouseDragged(MouseEvent e){
+                    x=e.getX();y=e.getY();
+                    repaint();
+                }
+                public void mouseMoved(MouseEvent e){
+                    x=y=t;
+                    repaint();
+                }
+            });
+        }
+        public void paint(Graphics g){
+            g.clearRect(0,0,s,s);
+            g.drawLine(t,t,x,y);
+        }
+    }
+    public static void main(String[] a){
+        new CanTes();
+    }
 }
