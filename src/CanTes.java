7a8c9aae800175cb90aaed370875947b609904dc
keisuke123 <luckey.star123@gmail.com>
2013-07-01 22:54:21 +0900

    Update CanTes.java

--- src/CanTes.java
+++ src/CanTes.java
@@ -1,36 +1,35 @@
 import java.awt.*;
 import java.awt.event.*;
 class CanTes extends Frame{
-    int s=600,x,y,t=300;
-    class W extends WindowAdapter{
-        public void windowClosing(WindowEvent e){System.exit(0);}
-    }
-    CanTes(){
-        addWindowListener(new W());
-        add(new C());
-        setSize(s,s);
-        setVisible(1>0);
-    }
-    class C extends Canvas{
-        C(){
-            x=y=t;
-            addMouseMotionListener(new MouseMotionListener(){
-                public void mouseDragged(MouseEvent e){
-                    x=e.getX();y=e.getY();
-                    repaint();
-                }
-                public void mouseMoved(MouseEvent e){
-                    x=y=t;
-                    repaint();
-                }
-            });
+  int s,x,y,t;
+  class W extends WindowAdapter{
+    public void windowClosing(WindowEvent e){System.exit(0);}
+  }
+  CanTes(){
+    setSize(s=2*(x=y=t=300),s);
+    addWindowListener(new W());
+    add(new C());
+    setVisible(1>0);
+  }
+  class C extends Canvas{
+    C(){
+      addMouseMotionListener(new MouseMotionListener(){
+        public void mouseDragged(MouseEvent e){
+          x=e.getX();y=e.getY();
+          repaint();
         }
-        public void paint(Graphics g){
-            g.clearRect(0,0,s,s);
-            g.drawLine(t,t,x,y);
+        public void mouseMoved(MouseEvent e){
+          x=y=t;
+          repaint();
         }
+      });
     }
-    public static void main(String[] a){
-        new CanTes();
+    public void paint(Graphics g){
+      g.clearRect(0,0,s,s);
+      g.drawLine(t,t,x,y);
     }
+  }
+  public static void main(String[] a){
+    new CanTes();
+  }
 }
